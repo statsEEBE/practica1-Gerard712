b96fea1b243b186b8b169fda824020675781f99c
gerard.gamiz <gerard.gamiz@auleseebe.upc.edu>
2025-02-18 09:58:26 +0100

    resolucion

--- Problema3.R
+++ Problema3.R
@@ -1,1 +1,13 @@
-#Codigo para problema 3
+#pregunta 3
+
+x <- 10:20 #numeros del 10 al 20
+
+x[2] #seleccionar numero
+x[2:4] #seleccionar varios
+x[-c(3,5,7)] #borrar numeros de esas posiciones
+
+sum(x)
+y<-x[-c(3,5,7)]
+y
+
+sum(exp(x)-exp(y))
